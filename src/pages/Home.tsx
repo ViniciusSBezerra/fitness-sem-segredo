c207e1962ef9272f7ed4522dc0ccdfd07781db43
vinicius souza <vinicius.souza@acct.global>
2023-12-23 00:21:07 -0300

    feature: open repositorie

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import "../index.css"
+
+
+export const HomePage = () => {
+    return (
+        <div className="container">
+
+            <h1>Fitness sem segredo</h1>
+            <h4>Em breve!</h4>
+        </div>
+    )
+}
